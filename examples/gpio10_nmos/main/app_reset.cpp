7e7b9f2f01c9230105dcff2f7512e8903c09f0b7
Mateusz Sztankiewicz <76174125+GodsCrisis@users.noreply.github.com>
2025-11-10 21:04:26 +0100

    Refactor app_reset.cpp to use button_gpio_cfg_t

--- examples/gpio10_nmos/main/app_reset.cpp
+++ examples/gpio10_nmos/main/app_reset.cpp
@@ -1,6 +1,7 @@
 #include <esp_log.h>
 #include <esp_matter.h>
 #include <iot_button.h>
+#include <button_gpio.h>
 #include <app_reset.h>
 #include <driver/gpio.h>
 
@@ -19,26 +20,24 @@ static void app_reset_button_callback(void *button_handle, void *usr_data)
 esp_err_t app_reset_button_register()
 {
 
-    
     button_config_t button_cfg = {
         .long_press_time = FACTORY_RESET_TIMEOUT_MS,
         .short_press_time = 0,
     };
     
 
-    gpio_button_config_t gpio_cfg = {
+    button_gpio_cfg_t gpio_cfg = {
         .gpio_num = RESET_BUTTON_GPIO,
         .active_level = 0,
     };
+    
 
-    extern const button_driver_t button_gpio_driver;
-    esp_err_t err = iot_button_create(&button_cfg, &button_gpio_driver, &gpio_cfg, &button_handle);
+    esp_err_t err = iot_button_create(&button_cfg, &button_gpio_driver, (void*)&gpio_cfg, &button_handle);
     
     if (err != ESP_OK || button_handle == NULL) {
         ESP_LOGE(TAG, "Failed to create button: %s", esp_err_to_name(err));
         return ESP_FAIL;
     }
-
 
     err = iot_button_register_cb(button_handle, BUTTON_LONG_PRESS_START, 
                                  NULL, app_reset_button_callback, NULL);
